a653bf6461acfedc7f6d4344f774bcc666a6e751
smako10 <sekimako10@gmail.com>
2020-06-10 17:55:17 +0900

    詳細画面表示まで対応

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,10 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
 
 class PostsController extends Controller
 {
     public function index() {
-        return views('posts.index');
+        // $posts = \App\Post::all();
+        // $posts = Post::all();
+        //$posts = Post::orderBy('created_at', 'desc')->get();
+        $posts = Post::latest()->get();
+        // $posts = [];
+        // dd($posts->toArray());
+        //return view('posts.index', ['posts' => $posts]);
+        return view('posts.index')->with('posts', $posts);
     }
+
+    // public function show($id) {
+    public function show(Post $post) {
+        // $post = Post::find($id);
+        // $post = Post::findOrFail($id);
+        return view('posts.show')->with('post', $post);
+    }
+
 }
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,17 +1,24 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <title>Blog Posts</title>
-</head>
-<body>
-    <div class="contaner">
-        <h1>Blog Posts</h1>
-        <ul>
-            <li><a href="">title</a></li>
-            <li><a href="">title</a></li>
-            <li><a href="">title</a></li>
-        </ul>
-    </div>
-</body>
-</html>
+@extends('layouts.default')
+
+{{--
+@section('title')
+Blog Posts
+@endsection
+--}}
+
+@section('title', 'Blog Posts')
+
+@section('content')
+    <h1>Blog Posts</h1>
+    <ul>
+        @forelse ($posts as $post)
+        <li><a href="/posts/{{ $post->id }}">{{ $post->title }}</a></li>
+        <!--<li><a href="{{ url('/posts', $post->id) }}">{{ $post->title }}</a></li>-->
+        <!-- Implicit Binding での書き方(ブラウザに表示してでないと機能しない) -->
+        <!-- controller渡し↓でないと↑は機能しないっぽい -->
+        <li><a href="{{ action('PostsController@show', $post) }}">{{ $post->title }}</a></li>
+        @empty
+        <li>No posts yet</li>
+        @endforelse
+    </ul>
+@endsection
