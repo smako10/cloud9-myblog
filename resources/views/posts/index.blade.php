33d43027fc297abc50e6a0abc3afda3ef83c10be
smako10 <sekimako10@gmail.com>
2020-06-10 15:39:36 +0900

    views up

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Blog Posts</title>
+</head>
+<body>
+    <div class="contaner">
+        <h1>Blog Posts</h1>
+        <ul>
+            <li><a href="">title</a></li>
+            <li><a href="">title</a></li>
+            <li><a href="">title</a></li>
+        </ul>
+    </div>
+</body>
+</html>
