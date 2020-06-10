ca87d1d6d24e3367babfc3c5c0c14c887eba1ae5
smako10 <66656685+smako10@users.noreply.github.com>
2020-06-10 18:18:18 +0900

    Update show.blade.php

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -4,6 +4,7 @@
 
 @section('content')
     <h1>
+        <!-- ブラウザなら↓も効く -->
         <a href="{{ url('/') }}" class="header-menu">Back</a>
         <!--<a href="/" class="header-menu">Back</a>-->
         {{ $post->title }}
