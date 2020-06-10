fae6807113b5495638e5772ed853adeea075fc75
smako10 <sekimako10@gmail.com>
2020-06-10 18:16:32 +0900

    Backリンクを追加

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -9,7 +9,10 @@ Blog Posts
 @section('title', 'Blog Posts')
 
 @section('content')
-    <h1>Blog Posts</h1>
+    <h1>
+        <a href="{{ url('/posts/create') }}" class="header-menu">New Post</a>
+        Blog Posts
+    </h1>
     <ul>
         @forelse ($posts as $post)
         <li><a href="/posts/{{ $post->id }}">{{ $post->title }}</a></li>
