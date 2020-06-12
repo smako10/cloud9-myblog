0bb1e69b499eaee61c552d1690a61e9b932878b6
smako10 <sekimako10@gmail.com>
2020-06-12 11:59:55 +0900

    新規画面と更新画面を追加

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Post;
+use App\Http\Requests\PostRequest;
 
 class PostsController extends Controller
 {
@@ -23,6 +24,29 @@ class PostsController extends Controller
         // $post = Post::find($id);
         // $post = Post::findOrFail($id);
         return view('posts.show')->with('post', $post);
+    }
+
+    public function create() {
+        return view('posts.create');
+    }
+
+    public function store(PostRequest $request) {
+        $post = new Post();
+        $post->title = $request->title;
+        $post->body = $request->body;
+        $post->save();
+        return redirect('/');
+    }
+
+    public function edit(Post $post) {
+        return view('posts.edit')->with('post', $post);
+    }
+
+    public function update(PostRequest $request, Post $post) {
+        $post->title = $request->title;
+        $post->body = $request->body;
+        $post->save();
+        return redirect('/');
     }
 
 }
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -16,11 +16,14 @@ Blog Posts
     </h1>
     <ul>
         @forelse ($posts as $post)
-        <li><a href="/posts/{{ $post->id }}">{{ $post->title }}</a></li>
+        <!--<li><a href="/posts/{{ $post->id }}">{{ $post->title }}</a></li>-->
         <!--<li><a href="{{ url('/posts', $post->id) }}">{{ $post->title }}</a></li>-->
         <!-- Implicit Binding での書き方(ブラウザに表示してでないと機能しない) -->
         <!-- controller渡し↓でないと↑は機能しないっぽい -->
-        <li><a href="{{ action('PostsController@show', $post) }}">{{ $post->title }}</a></li>
+        <li>
+            <a href="{{ action('PostsController@show', $post) }}">{{ $post->title }}</a>
+            <a href="{{ action('PostsController@edit', $post) }}" class="edit">[Edit]</a>
+        </li>
         @empty
         <li>No posts yet</li>
         @endforelse
